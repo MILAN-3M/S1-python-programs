a04224a64f36803c07b088ba288913900be48feb
MILAN-3M <56360424+MILAN-3M@users.noreply.github.com>
2019-10-09 22:36:28 +0530

    Update 32.py

--- 32.py
+++ 32.py
@@ -1,12 +1,20 @@
 string=str(input("enter the string:"))
 vowel=0
 consonent=0
+non=0
 for i in string:
-	if i=="a" or i=="e" or i=="o" or i=="i" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U":
-		vowel=vowel+1
+	int c;
+	c=i;
+	if c>=65 and c<=90 or c>=97 and c<=122:
+			
+		if i=="a" or i=="e" or i=="o" or i=="i" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U":
+			vowel=vowel+1
 
+		else:
+			consonent=consonent+1
 	else:
-		consonent=consonent+1
+		non+=1
 print("no of vowels=",vowel)
 print("no of non vowels=",consonent)
+print("No of non alphabets=",non)
 	
